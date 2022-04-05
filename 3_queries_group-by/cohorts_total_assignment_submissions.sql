f2f33f5cfaee7de538c91354f36822b7ce687c1c
amuliawijaya23 <amuliawijaya@gmail.com>
2022-04-04 23:18:03 -0700

    add group by queries

---
+++ 3_queries_group-by/cohorts_total_assignment_submissions.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students
+ON sutdents.id = assignment_submissions.student_id
+JOIN cohorts
+ON cohorts.id = students.cohort_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
--- 3_queries_group-by/large_cohorts.sql
+++ 3_queries_group-by/large_cohorts.sql
@@ -1,1 +1,7 @@
-  
+  SELECT cohorts.name as cohort_name, count(students.*) as student_count
+  FROM students
+  JOIN cohorts
+  ON cohorts.id = students.cohort_id
+  GROUP BY cohort_name
+  HAVING count(students.*) > 17
+  ORDER BY student_count;
